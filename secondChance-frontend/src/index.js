d83f065890e67f21708a96eaa1e313ea49183c50
Damir <109801219+EZDAMIR@users.noreply.github.com>
2026-09-12 17:01:59 +0500

    Support project path routing on GitHub Pages

--- secondChance-frontend/src/index.js
+++ secondChance-frontend/src/index.js
@@ -7,8 +7,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-        <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
     </Router>
   </React.StrictMode>
 );
